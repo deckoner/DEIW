fcfdc2a2b27a75283e4f382142b4fa1fc5c27ec4
Deckoner <58949309+deckoner@users.noreply.github.com>
2023-11-15 21:19:40 +0100

    Animaciones

--- proyecto/src/js/util.js
+++ proyecto/src/js/util.js
@@ -1,9 +1,15 @@
-const listaDigimones = ["Lucemon", "Terriermon","Gomamon", "Tentomon","Lopmon"];
+const listaDigimones = [
+    "Lucemon",
+    "Terriermon",
+    "Gomamon",
+    "Tentomon",
+    "Lopmon",
+];
 const direccionPrincipal = window.location.origin;
 
 function digimonFavorito() {
     const nAleatorio = Math.floor(Math.random() * listaDigimones.length);
-    const rutaDigimones = direccionPrincipal+"/src/js/json/";
+    const rutaDigimones = direccionPrincipal + "/src/js/json/";
     const nombreArchivo = listaDigimones[nAleatorio] + ".json";
     const rutaCompleta = rutaDigimones + nombreArchivo;
 
@@ -31,21 +37,74 @@ function imprimirDatosDigimon(digimon) {
     const imagenElement = document.createElement("img");
     imagenElement.src = digimon.images[0].href; // Acceder al primer elemento del array de imágenes
     imagenElement.alt = "Imagen de " + digimon.name;
-    imagenElement.className = "imagenAnimacion"
+    imagenElement.className = "imagenAnimacion";
 
     // Agregar los elementos al div
     divDigimon.appendChild(nombreElement);
     divDigimon.appendChild(imagenElement);
 }
 
-document.addEventListener('DOMContentLoaded', (event) => {
-    digimonFavorito()
+function setCookie(cname, cvalue, exdays) {
+    const d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    let expires = "expires="+ d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+ }
 
-    document.getElementById('email').addEventListener('submit', function(event) {
-        event.preventDefault();
-        // Aquí va el código para enviar el correo
-        this.reset();
+ function getCookie(cname) {
+    let name = cname + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    for(let i = 0; i <ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+ }
 
-        alert("Email enviado con exito")
-       });
-});
+ document.addEventListener("DOMContentLoaded", (event) => {
+    const cookies = document.getElementById("cookies");
+    const overlay = document.getElementById("overlay");
+    const aceptarCookies = document.getElementById("aceptarCookies");
+    const rechazarCookies = document.getElementById("rechazarCookies");
+  
+    if (getCookie('cookiesAceptadas') === 'true') {
+        cookies.style.visibility = "hidden";
+        overlay.style.display = "none"; // Ocultar el overlay cuando las cookies son aceptadas
+        document.body.style.overflow = 'auto'; // Habilitar el desplazamiento cuando las cookies son aceptadas
+    }
+   
+    digimonFavorito();
+   
+    // Verifica si la página actual es "contactame.html"
+    if (window.location.pathname === "/contactame.html") {
+        document
+            .getElementById("email")
+            .addEventListener("submit", function (event) {
+                event.preventDefault();
+                // Aquí va el código para enviar el correo
+                this.reset();
+   
+                alert("Email enviado con exito");
+            });
+    }
+   
+    aceptarCookies.addEventListener("click", function () {
+        cookies.style.visibility = "hidden";
+        overlay.style.display = "none"; // Ocultar el overlay cuando el usuario acepta las cookies
+        document.body.style.overflow = 'auto'; // Habilitar el desplazamiento cuando el usuario acepta las cookies
+        setCookie('cookiesAceptadas', 'true', 1);
+    });
+   
+    rechazarCookies.addEventListener("click", function () {
+        cookies.style.visibility = "hidden";
+        overlay.style.display = "none"; // Ocultar el overlay cuando el usuario rechaza las cookies
+        document.body.style.overflow = 'auto'; // Habilitar el desplazamiento cuando el usuario rechaza las cookies
+        setCookie('cookiesAceptadas', 'false', 1);
+    });
+  });
